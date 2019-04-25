884eb520d886b7b7130d4db873aa53692f66fe49
Akere <adetolaakere1@gmail.com>
2019-04-25 04:26:15 +0100

    feat(admin-can-view-unrepaid-loans endpoint):admin view unrepaid loans
    - This feature allows an admin to view unrepaid loans
    - Unrepaid loans are loans with status approved and repaid as false
    
    [Delivers #165572741]

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,9 +1,9 @@
 import models from "../model/userData";
 import moment from "moment";
-import validation from '../validation/validation';
-import Auth from '../middleware/isAuth';
-import bcrypt from 'bcryptjs';
-import helper from '../helper/users';
+import validation from "../validation/validation";
+import Auth from "../middleware/isAuth";
+import bcrypt from "bcryptjs";
+import helper from "../helper/users";
 
 const { genSaltSync, hashSync, compareSync } = bcrypt;
 
@@ -19,118 +19,107 @@ class userController {
     if (error)
       return res.status(422).json({ message: error.details[0].message });
 
-        try{
-            // check if user already exists
-      const emailExists = models.Users.find(user => user.email === req.body.email);
+    try {
+      // check if user already exists
+      const emailExists = models.Users.find(
+        user => user.email === req.body.email
+      );
 
       if (emailExists) {
         return res.status(409).json({
           status: 409,
-          error: 'user already exists',
+          error: "user already exists"
         });
       }
-
 
       const salt = bcrypt.genSaltSync(10);
       const hash = hashSync(body.password, salt);
 
-      
-
       const usersLength = models.Users.length;
-    const lastUserId = models.Users[usersLength - 1].id;
-    
-    const post = {
-      id: lastUserId + 1,
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      password: bcrypt.hashSync(body.password, salt),
-      status: 'unverified',
-      address:req.body.address,
-      isAdmin: 'false',
-      createdOn: moment(new Date()),
-      modifiedOn: moment(new Date())
-    };
-  
-    models.Users.push(post);
-    const token = Auth.generateToken(models.Users[0].id);
-    
-    return res.status(201).json({
-      status: 200,
-      data: [
-        {
-          
-          token,
-          post,
-          
-        },
-      ],
-    }
-    );
+      const lastUserId = models.Users[usersLength - 1].id;
 
-  }
-    catch (e) {
+      const post = {
+        id: lastUserId + 1,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: bcrypt.hashSync(body.password, salt),
+        status: "unverified",
+        address: req.body.address,
+        isAdmin: "false",
+        createdOn: moment(new Date()),
+        modifiedOn: moment(new Date())
+      };
+
+      models.Users.push(post);
+      const token = Auth.generateToken(models.Users[0].id);
+
+      return res.status(201).json({
+        status: 200,
+        data: [
+          {
+            token,
+            post
+          }
+        ]
+      });
+    } catch (e) {
       return res.status(400).json({
         status: 400,
-        error: 'Sorry, something went wrong, try again',
+        error: "Sorry, something went wrong, try again"
       });
     }
   }
 
-    /**
+  /**
    * login a user
    * @param {*} req
    * @param {*} res
    */
   static loginUser(req, res) {
-
     const { body } = req;
     // check if user pass valid and required data
     const { error } = validation.validateLogin(req.body);
     if (error) {
       return res.status(422).json({
         status: 422,
-        message: error.details[0].message,
+        message: error.details[0].message
       });
     }
 
     //  find user by email
 
-     const userExists = models.Users.find(user => user.email === req.body.email);
+    const userExists = models.Users.find(user => user.email === req.body.email);
 
-     // check if user exists in our data structure
-     if (!userExists) {
-       return res.status(404).json({
-         status: 404,
-         error: 'User does not exist',
-       });
-     }
-     
-     const hashedPassword = userExists.password;
+    // check if user exists in our data structure
+    if (!userExists) {
+      return res.status(404).json({
+        status: 404,
+        error: "User does not exist"
+      });
+    }
 
-     // check if user provided password matches existing password
+    const hashedPassword = userExists.password;
+
+    // check if user provided password matches existing password
     if (!bcrypt.compareSync(body.password, hashedPassword)) {
       return res.status(401).json({
         status: 401,
-        error: 'Invalid email/password',
+        error: "Invalid email/password"
       });
     }
-       // get generated token
-       const token = Auth.generateToken(models.Users[0].id);
+    // get generated token
+    const token = Auth.generateToken(models.Users[0].id);
 
-     return res.status(200).json({
-       status: 200,
-       data: [
-         {
-           token,
-         },
-       ],
-     });
-    }
-
+    return res.status(200).json({
+      status: 200,
+      data: [
+        {
+          token
+        }
+      ]
+    });
   }
-
-
-
+}
 
 export default userController;
--- server/model/userData.js
+++ server/model/userData.js
@@ -1,96 +1,104 @@
-let models =
-{
+let models = {
   Users: [
     {
       id: 1,
-      email: 'firstuser@gmail.com',
-      firstName: 'John',
-      lastName: 'Doe',
-      password: '$2a$10$gEwkPvCIWRAzwhbMG3WPju0QxCOsqznvaw9h3nAFBug9cidLfeNMq',
-      status: 'verified',
-      Address: '20,client',
+      email: "firstuser@gmail.com",
+      firstName: "John",
+      lastName: "Doe",
+      password: "$2a$10$gEwkPvCIWRAzwhbMG3WPju0QxCOsqznvaw9h3nAFBug9cidLfeNMq",
+      status: "verified",
+      Address: "20,client",
       isAdmin: false,
-      createdOn: '2019-04-21T17:00:47.094Z',
-      modifiedOn: '2019-04-21T17:00:47.096Z'
+      createdOn: "2019-04-21T17:00:47.094Z",
+      modifiedOn: "2019-04-21T17:00:47.096Z"
     },
     {
       id: 4,
-      email: 'alaksol@yahoo.com',
-      firstName: 'Adetolaasd',
-      lastName: 'Akerele',
-      password: 'Sweetmum',
-      status: 'verified',
-      Address: '21,Okusaga Street, Ilupeju',
+      email: "alaksol@yahoo.com",
+      firstName: "Adetolaasd",
+      lastName: "Akerele",
+      password: "Sweetmum",
+      status: "verified",
+      Address: "21,Okusaga Street, Ilupeju",
       isAdmin: false,
-      createdOn: '2019-04-21T17:11:37.631Z',
-      modifiedOn: '2019-04-21T17:00:47.096Z'
+      createdOn: "2019-04-21T17:11:37.631Z",
+      modifiedOn: "2019-04-21T17:00:47.096Z"
     },
-
 
     {
       id: 6,
-      email: 'dimeji@gmail.com',
-      firstName: 'Dimeji',
-      lastName: 'Akere',
-      password: 'Sweetmum',
-      Status: 'unverified',
-      Address: '20,okusaga',
+      email: "dimeji@gmail.com",
+      firstName: "Dimeji",
+      lastName: "Akere",
+      password: "Sweetmum",
+      Status: "unverified",
+      Address: "20,okusaga",
       isAdmin: false,
-      createdOn: '2019-04-21T17:11:37.631Z',
-      modifiedOn: '2019-04-21T17:00:47.096Z'
+      createdOn: "2019-04-21T17:11:37.631Z",
+      modifiedOn: "2019-04-21T17:00:47.096Z"
     },
     {
       id: 7,
-      email: 'bayomi@gmail.com',
-      firstName: 'suly',
-      lastName: 'bayo',
-      password: '$2a$10$yUHclZyx9saFA7JX7bCr1u4OVnS4Z2i5JvzZexHRK/0SxKDgWoCbO',
-      status: 'verified',
-      Address: '20,okusaga',
+      email: "bayomi@gmail.com",
+      firstName: "suly",
+      lastName: "bayo",
+      password: "$2a$10$yUHclZyx9saFA7JX7bCr1u4OVnS4Z2i5JvzZexHRK/0SxKDgWoCbO",
+      status: "verified",
+      Address: "20,okusaga",
       isAdmin: true,
-      createdOn: '2019-04-22T11:26:27.010Z',
-      modifiedOn: '2019-04-22T11:26:27.011Z'
-    },
-
-
+      createdOn: "2019-04-22T11:26:27.010Z",
+      modifiedOn: "2019-04-22T11:26:27.011Z"
+    }
   ],
-  Loans: [{
-    id: 1,
-    user: "firstuser@gmail.com",
-    createdOn: "2019-04-22T14:34:25.265Z",
-    status: "pending",
-    repaid: false,
-    tenor: 3,
-    amount: 400000,
-    paymentInstallment: 140000,
-    balance: 420000,
-    interest: 20000
-  },
-  {
-    id: 2,
-    user: "seun@gmail.com",
-    createdOn: "2019-04-22T14:34:25.265Z",
-    status: "approved",
-    repaid: true,
-    tenor: 3,
-    amount: 800000,
-    paymentInstallment: 280000,
-    balance: 840000,
-    interest: 40000
-  },
-  {
-    id: 3,
-        user: "firstuser@gmail.com",
-        createdOn: "2019-04-24T09:38:09.085Z",
-        status: "pending",
-        repaid: false,
-        tenor: 5,
-        amount: 700000,
-        paymentInstallment: 147000,
-        balance: 735000,
-        interest: 35000
-  }
-
+  Loans: [
+    {
+      id: 1,
+      user: "firstuser@gmail.com",
+      createdOn: "2019-04-22T14:34:25.265Z",
+      status: "pending",
+      repaid: false,
+      tenor: 3,
+      amount: 400000,
+      paymentInstallment: 140000,
+      balance: 420000,
+      interest: 20000
+    },
+    {
+      id: 2,
+      user: "bayomi@gmail.com",
+      createdOn: "2019-04-22T14:34:25.265Z",
+      status: "approved",
+      repaid: true,
+      tenor: 3,
+      amount: 800000,
+      paymentInstallment: 280000,
+      balance: 840000,
+      interest: 40000
+    },
+    {
+      id: 3,
+      user: "firstuser@gmail.com",
+      createdOn: "2019-04-24T09:38:09.085Z",
+      status: "approved",
+      repaid: true,
+      tenor: 5,
+      amount: 700000,
+      paymentInstallment: 147000,
+      balance: 735000,
+      interest: 35000
+    },
+    {
+      id: 4,
+      user: "firstuser@gmail.com",
+      createdOn: "2019-04-24T23:43:10.644Z",
+      status: "approved",
+      repaid: false,
+      tenor: 5,
+      amount: 600000,
+      paymentInstallment: 210000,
+      balance: 630000,
+      interest: 30000
+    }
   ]
 };
 
