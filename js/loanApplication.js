8246449c48bb0ee18054a91533640f239d88b096
Akere <adetolaakere1@gmail.com>
2019-06-05 16:50:31 +0100

    Updates

--- js/loanApplication.js
+++ js/loanApplication.js
@@ -7,15 +7,15 @@ const checkExpiredToken = responseBody => {
     }, 1000);
   }
 };
-const resetFields = () => {
-  const fields = document.querySelectorAll(".error");
-  const fieldsArr = Array.prototype.slice.call(fields);
-  fieldsArr.forEach(element => {
-    const currentField = element;
-    currentField.innerHTML = "";
-    currentField.previousElementSibling.style.border = "1px solid #f4f4f4";
-  });
-};
+// const resetFields = () => {
+//   const fields = document.querySelectorAll(".error");
+//   const fieldsArr = Array.prototype.slice.call(fields);
+//   fieldsArr.forEach(element => {
+//     const currentField = element;
+//     currentField.innerHTML = "";
+//     currentField.previousElementSibling.style.border = "1px solid #f4f4f4";
+//   });
+// };
 
 const displayFeedback = responseData => {
   let listItem = "";
@@ -39,23 +39,22 @@ const displayFeedback = responseData => {
 
 const postLoanApp = e => {
   e.preventDefault();
-  resetFields();
+  // resetFields();
 
   // get all user input values
   const loanTenor = document.getElementById("tenor").value;
   const loanAmount = document.getElementById("amount").value;
   const feedbackContainer = document.querySelector(".feedback-message");
 
-  // sign up API-endpoint url
-  const url = "https://my-quick-credit-app.herokuapp.com/api/v2/loans";
-
   // User input data object
   const formData = {
     clientemail: body.data.loan.clientemail,
-    createdon: new Date(),
-    tenor: parseInt(loanTenor, 12),
-    amount: parseInt(loanAmount, 10)
+    tenor: parseInt(loanTenor, 10),
+    amount: parseFloat(loanAmount)
   };
+
+  // sign up API-endpoint url
+  const url = "https://my-quick-credit-app.herokuapp.com/api/v2/loans";
 
   // get user object from
   let userToken = "";
@@ -77,23 +76,19 @@ const postLoanApp = e => {
   })
     .then(res => res.json())
     .then(body => {
-      hideOverlay();
-
       // check for success status
       if (body.status === 201) {
-        feedbackContainer.innerHTML = "loa application sent successfully";
+        feedbackContainer.innerHTML = "loan application sent successfully";
         feedbackContainer.classList.remove("feedback-message-error");
         feedbackContainer.classList.add("feedback-message-success");
-        window.scrollTo(0, 0);
 
         // Redirect user to home page
         setTimeout(() => {
-          window.location.href = "index.html";
+          window.location.href = "sign-in.html";
         }, 1000);
       } else {
-        feedbackContainer.innerHTML = displayFeedback(body);
+        feedbackContainer.innerHTML = "error";
         feedbackContainer.classList.add("feedback-message-error");
-        window.scrollTo(0, 0);
 
         // redirect to login if token has expired
         checkExpiredToken(body);
@@ -102,6 +97,6 @@ const postLoanApp = e => {
     .catch(err => err);
 };
 
-const postQuestionBtn = document.getElementById("post-loann");
+const postLoanBtn = document.getElementById("post-loann");
 
-postQuestionBtn.addEventListener("click", postLoanApp);
+postLoanBtn.addEventListener("click", postLoanApp);
