a6bfc2239ae6804e05bf79420f27a395e91a58fb
Akere <adetolaakere1@gmail.com>
2019-07-01 23:24:18 +0100

    Updates

--- js/viewAllLoans.js
+++ js/viewAllLoans.js
@@ -59,7 +59,7 @@ const getAllApplications = () => {
               </article>
             <article>
               <p>Application Date</p>
-              <p ${formatedDate}></p>
+              <p >${formatedDate}</p>
             </article>
                 <article class ="buttons">
                     
