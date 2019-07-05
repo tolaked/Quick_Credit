b617e5910fce47fc752139fcb22cee3f43b9f81a
Akere <adetolaakere1@gmail.com>
2019-07-05 19:15:50 +0100

    feat(consume-loan-approval):consume loan approval
    - this feature allows an admin to approve loan applications
    
    [finishes #167089810]

--- UI/js/viewAllLoans.js
+++ UI/js/viewAllLoans.js
@@ -1,5 +1,4 @@
 const feedbackContainer = document.querySelector(".feedback-message");
-
 const checkExpiredToken = responseBody => {
   if (responseBody.error.expiredAt) {
     // Redirect user to home page
@@ -61,7 +60,9 @@ const getAllApplications = () => {
                 <article class ="buttons">
                     
                   </article>
-                  <div class="acctt"><a class="acctt" href="loan.html">View Application</a></div>
+                  <div class="${userLoan.id}" id="acctti"><a class="${
+            userLoan.id
+          }" id="acctt" href="loan.html">View Application</a></div>
                 <hr>`;
         });
 
@@ -77,84 +78,5 @@ const getAllApplications = () => {
     .catch(err => err);
 };
 
-const getId = () => {
-  const urlString = window.location.href;
-  const url = new URL(urlString);
-  const oneloanId = url.searchParams.get("id");
-
-  return oneloanId;
-};
-
-const loanDetails = () => {
-  const loanID = getId();
-  // All loans endpoint url
-  const detailsurl = `https://my-quick-credit-app.herokuapp.com/api/v2/loans/${loanID}`;
-
-  let detailsToken;
-  if (localStorage.getItem("user")) {
-    const userData = JSON.parse(localStorage.getItem("user"));
-    const { token } = userData;
-    detailsToken = token;
-  }
-
-  // make a GET request to meetups
-  fetch(detailsurl, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      token: detailsToken
-    }
-  })
-    .then(res => res.json())
-    .then(body => {
-      // hideOverlay();
-      let loandetails = "";
-      loandetails += ` <article>
-          <p>Client's email</p>
-          <p>${body.data[0].clientemail}</p>
-        </article>
-        <article  id="${body.data[0].id}" value="${body.data[0].id}">
-                <p>loan id</p>
-                <p>${body.data[0].id}</p>
-              </article>
-        <article>
-                <p>Amount</p>
-                <p>&#8358;${body.data[0].amount}</p>
-              </article>
-        <article>
-          <p>Tenor</p>
-          <p>${body.data[0].tenor}</p>
-        </article>
-        <article>
-          <p>Balance</p>
-          <p>&#8358;${body.data[0].balance}</p>
-        </article>
-        <article>
-            <p>Interest</p>
-            <p>${body.data[0].interest}</p>
-          </article>
-          <article>
-              <p>Payment Installment</p>
-              <p>&#8358;${body.data[0].paymentinstallment}</p>
-            </article>
-        <article class ="buttons">
-          <p>Status</p>
-          <p>${body.data[0].status}</p>
-        </article>
-        <article >
-                <button class="ref" id="approv">Approve</button>
-                <button class="ref" id="reject">Reject</button>
-              </article>`;
-
-      // get loan container
-      const getFulldetails = document.getElementById("fulldetails");
-
-      // Display all loan record
-      getFulldetails.innerHTML = loandetails;
-    })
-    .catch(err => err);
-};
-
 // fetch all loan record
 getAllApplications();
-loanDetails();
