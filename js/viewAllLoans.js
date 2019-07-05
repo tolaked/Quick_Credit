17686d05b1e63c5bee41739c80f9ea748301e744
Akere <adetolaakere1@gmail.com>
2019-07-05 13:37:17 +0100

    Updates

--- js/viewAllLoans.js
+++ js/viewAllLoans.js
@@ -87,81 +87,81 @@ const getAllApplications = e => {
 //   return oneloanId;
 // };
 
-const loanDetails = e => {
-  const loanID = e.target.className;
-  // All loans endpoint url
-  const detailsurl = `https://my-quick-credit-app.herokuapp.com/api/v2/loans/${loanID}`;
+// const loanDetails = e => {
+//   // const loanID = e.target.className;
+//   // All loans endpoint url
+//   const detailsurl = `https://my-quick-credit-app.herokuapp.com/api/v2/loans/${loanID}`;
 
-  let detailsToken;
-  if (localStorage.getItem("user")) {
-    const userData = JSON.parse(localStorage.getItem("user"));
-    const { token } = userData;
-    detailsToken = token;
-  }
+//   let detailsToken;
+//   if (localStorage.getItem("user")) {
+//     const userData = JSON.parse(localStorage.getItem("user"));
+//     const { token } = userData;
+//     detailsToken = token;
+//   }
 
-  // make a GET request to meetups
-  fetch(detailsurl, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      token: detailsToken
-    }
-  })
-    .then(res => res.json())
-    .then(body => {
-      // hideOverlay();
-      if (body.status === 200) {
-        let loandetails = "";
-        loandetails += ` <article>
-          <p>Client's email</p>
-          <p>${body.data[0].clientemail}</p>
-        </article>
-        <article  id="${body.data[0].id}" value="${body.data[0].id}">
-                <p>loan id</p>
-                <p>${body.data[0].id}</p>
-              </article>
-        <article>
-                <p>Amount</p>
-                <p>&#8358;${body.data[0].amount}</p>
-              </article>
-        <article>
-          <p>Tenor</p>
-          <p>${body.data[0].tenor}</p>
-        </article>
-        <article>
-          <p>Balance</p>
-          <p>&#8358;${body.data[0].balance}</p>
-        </article>
-        <article>
-            <p>Interest</p>
-            <p>${body.data[0].interest}</p>
-          </article>
-          <article>
-              <p>Payment Installment</p>
-              <p>&#8358;${body.data[0].paymentinstallment}</p>
-            </article>
-        <article class ="buttons">
-          <p>Status</p>
-          <p>${body.data[0].status}</p>
-        </article>
-        <article >
-                <button class="ref" id="approv">Approve</button>
-                <button class="ref" id="reject">Reject</button>
-              </article>`;
+//   // make a GET request to meetups
+//   fetch(detailsurl, {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       token: detailsToken
+//     }
+//   })
+//     .then(res => res.json())
+//     .then(body => {
+//       // hideOverlay();
+//       if (body.status === 200) {
+//         let loandetails = "";
+//         loandetails += ` <article>
+//           <p>Client's email</p>
+//           <p>${body.data[0].clientemail}</p>
+//         </article>
+//         <article  id="${body.data[0].id}" value="${body.data[0].id}">
+//                 <p>loan id</p>
+//                 <p>${body.data[0].id}</p>
+//               </article>
+//         <article>
+//                 <p>Amount</p>
+//                 <p>&#8358;${body.data[0].amount}</p>
+//               </article>
+//         <article>
+//           <p>Tenor</p>
+//           <p>${body.data[0].tenor}</p>
+//         </article>
+//         <article>
+//           <p>Balance</p>
+//           <p>&#8358;${body.data[0].balance}</p>
+//         </article>
+//         <article>
+//             <p>Interest</p>
+//             <p>${body.data[0].interest}</p>
+//           </article>
+//           <article>
+//               <p>Payment Installment</p>
+//               <p>&#8358;${body.data[0].paymentinstallment}</p>
+//             </article>
+//         <article class ="buttons">
+//           <p>Status</p>
+//           <p>${body.data[0].status}</p>
+//         </article>
+//         <article >
+//                 <button class="ref" id="approv">Approve</button>
+//                 <button class="ref" id="reject">Reject</button>
+//               </article>`;
 
-        // get loan container
-        const getFulldetails = document.getElementById("fulldetails");
+//         // get loan container
+//         const getFulldetails = document.getElementById("fulldetails");
 
-        // Display all loan record
-        getFulldetails.innerHTML = loandetails;
-      } else {
-        loanDetailsFedback.innerHTML = displayFeedback(body);
-        feedbackContainer.classList.add("feedback-message-error");
-      }
-    })
-    .catch(err => err);
-};
+//         // Display all loan record
+//         getFulldetails.innerHTML = loandetails;
+//       } else {
+//         loanDetailsFedback.innerHTML = displayFeedback(body);
+//         feedbackContainer.classList.add("feedback-message-error");
+//       }
+//     })
+//     .catch(err => err);
+// };
 
 // fetch all loan record
 getAllApplications();
-loanDetails();
+// loanDetails();
